e5f41c1b86e1a8843e578e734e4bf61f60c00992
Pratham <prathamsharmafaangm@gmail.com>
2022-11-12 13:32:13 +0530

    Complete stripe payment integration

--- client/pages/api/stripe.js
+++ client/pages/api/stripe.js
@@ -11,11 +11,13 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1M34sYSEADOwlSFPnMj0QoX7' },
+          { shipping_rate: 'shr_1M34spSEADOwlSFPLTWFuSrL' },
+
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/idaba861/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
